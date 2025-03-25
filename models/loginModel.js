63b27c6a02c30c5e7c2ad146d40f6ea0b3a55446
ElMilaneso-69 <zs22016112@estudiantes.uv.mx>
2025-03-25 16:29:47 -0600

    Cifrado en el login

--- models/loginModel.js
+++ models/loginModel.js
@@ -1,4 +1,5 @@
 import { db } from '../db/connection.js';
+import bcrypt from 'bcrypt';
 
 export const getUserByEmail = async (email) => {
   const [rows] = await db.query('SELECT * FROM usuarios WHERE correo = ?', [email]);
@@ -6,5 +7,5 @@ export const getUserByEmail = async (email) => {
 };
 
 export const validatePassword = async (password, storedPassword) => {
-  return password === storedPassword;
+  return await bcrypt.compare(password, storedPassword);
 };
