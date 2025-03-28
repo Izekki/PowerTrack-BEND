b47330ca6c381e54e44914de38d1cd1119eb4a43
Izekki <95318835+Izekki@users.noreply.github.com>
2025-03-28 01:17:53 -0600

    edit groupcontroller funtion creategroups, add error message

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -13,9 +13,13 @@ export const createGroups = async (req, res) => {
         const newGroup = await createGroup(name, devices);
         res.status(201).json({ message: "Grupo creado exitosamente", newGroup });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        if (error.message.includes("Ya existe un grupo con ese nombre")) {
+            return res.status(400).json({ message: error.message });  // Responder con 400 en lugar de 500
+        }
+        res.status(500).json({ error: "Error interno del servidor" });
     }
 };
+
 
 export const getAllGroups = async (req, res) => {
     try {
