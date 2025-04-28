b975a38ba161e15cbaa1418ac09e40cec64a9b55
Izekki <95318835+Izekki@users.noreply.github.com>
2025-04-27 21:25:20 -0600

    update sensor sensorRouter

--- routes/sensorRouter.js
+++ routes/sensorRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getSensors,getSensorById, verifySensor } from '../controllers/sensorController.js';
-import { saveMeasurement } from '../models/measurementModel.js';
+import { createMeasurement } from '../controllers/measurementController.js';
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/obtener', getSensors);
 router.get('/byId/:id',getSensorById)
 router.post('/verify',verifySensor)
-router.post('/measurements',saveMeasurement)
+router.post('/measurements',createMeasurement)
 
 
 export default router;
