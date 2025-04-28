78252bbd46e9455c8b9bd400a95297e825feab35
Izekki <95318835+Izekki@users.noreply.github.com>
2025-04-27 21:18:00 -0600

    `Added saveMeasurement route to sensorRouter.js`

--- routes/sensorRouter.js
+++ routes/sensorRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getSensors,getSensorById, verifySensor } from '../controllers/sensorController.js';
+import { saveMeasurement } from '../models/measurementModel.js';
 
 const router = express.Router();
 
@@ -7,6 +8,7 @@ const router = express.Router();
 router.get('/obtener', getSensors);
 router.get('/byId/:id',getSensorById)
 router.post('/verify',verifySensor)
+router.post('/measurements',saveMeasurement)
 
 
 export default router;
